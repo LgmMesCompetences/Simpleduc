f6f087c795fbce7d7e833c9e06fe6fe22673ea88
Rémi <remi.becquaert35@gmail.com>
2021-09-21 17:43:58 +0200

    inscrire controler creation and begin of updateMDP

--- src/controleur/securityControleur.php
+++ src/controleur/securityControleur.php
@@ -1,5 +1,39 @@
 <?php
 
+function inscrireControleur($twig, $db){
+
+	$form = array();
+	if (isset($_POST['btInscrire'])){
+		$email = $_POST['email'];
+		$password = $_POST['password'];
+		$password2 = $_POST['password2'];
+		$nom = $_POST['nom'];
+		$prenom = $_POST['prenom'];
+		$dateEmbauche = $_POST['dateEmbauche'];
+		$fonction = $_POST['fonction'];
+		$form['valide'] = true;
+		if ($password!=$password2){
+			$form['valide'] = false;
+			$form['message'] = 'Les mots de passe sont différents !';
+		}
+		else{
+			$utilisateur = new User($db);
+			$exec = $utilisateur->insert($email, password_hash($password, PASSWORD_DEFAULT), $nom, $prenom, $dateEmbauche, $fonction);
+			if (!$exec){
+				$form['valide'] = false;
+				$form['message'] = 'Problème de création de compte !';
+			}
+		$form['email'] = $email;
+		$form['fonction'] = $fonction;		
+		}
+		echo $twig->render('security3/ajoutCompte.html.twig');
+	}
+}
+
+//TODO AVANT faire la requête d'inscription dans le modele !!!!!!
+//TODO faire la vue d'inscription !!!!!
+//TODO créer la fonction updateMDP !!!!!!!!!!!
+
 function securityControleur($twig, $db) {
 	$form = array();
 
@@ -25,7 +59,7 @@ function securityControleur($twig, $db) {
 			$form['message'] = 'Login ou mot de passe incorrect !';
 		}
 	}
-	echo $twig->render('security.html.twig', array());
+	echo $twig->render('security/connexion.html.twig', array());
 }
 
 function deconnexionControleur($twig, $db){
